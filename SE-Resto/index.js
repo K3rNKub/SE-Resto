17c3e651020152e6c2a54e0e2518dc2f6831b045
K3rNKub <111554758+K3rNKub@users.noreply.github.com>
2023-02-16 16:37:05 +0700

    Add files via upload

---
+++ SE-Resto/index.js
@@ -0,0 +1,34 @@
+let menu = document.querySelector("#Menu-bars");
+let navbar = document.querySelector("header .flex .navbar");
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+}
+// Show search
+let search = document.querySelector("#search-icon");
+search.onclick = () =>{
+    let search_form = document.querySelector("#search-form")
+    search_form.classList.toggle('active');
+}
+// Hide search
+let close_icon = document.querySelector("#close");
+close_icon.onclick = () => {
+  let search_form = document.querySelector("#search-form");
+  search_form.classList.remove("active");
+};
+
+// Swiper funtion
+var swiper = new Swiper(".home-slider",{
+  spaceBetween:30,
+  centeredSlides:true,
+  autoplay:{
+    delay:3000,
+    disableOnInteraction:false,
+  },
+  pagination :{
+    el:".swiper-pagination",
+    clickable:true,
+  },
+  loop:true
+});
